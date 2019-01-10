a812db35f972f3e7229f5846d324874e65099491
Jacob Cedar <jcedar8640@bths.edu>
2019-01-10 10:40:05 -0500

    added stuf

--- src/JacobCedar/Deck.java
+++ src/JacobCedar/Deck.java
@@ -1,5 +1,16 @@
 package JacobCedar;
-
+import java.util.*;
 public class Deck {
+    private ArrayList<Card> unDealt = new ArrayList<Card>();
+    private ArrayList<Card> dealt = new ArrayList<Card>();
 
+    public Deck(String[] rank, String[] suit, int[] pointValue){
+        for(int i =0; i < pointValue.length; i++){
+            unDealt.add(new Card(rank[i], suit[i], pointValue[i]));
+        }
+
+    }
+    public boolean isEmpty(){
+        return unDealt.size() == 0;
+    }
 }
