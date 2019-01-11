08c8bf1104d584351a6e8c95168ef62113ef1023
Jacob Cedar <jcedar8640@bths.edu>
2019-01-11 11:01:21 -0500

    Finished Card and Deck

--- src/JacobCedar/Deck.java
+++ src/JacobCedar/Deck.java
@@ -13,4 +13,30 @@ public class Deck {
     public boolean isEmpty(){
         return unDealt.size() == 0;
     }
+    public int size(){
+        return unDealt.size();
+    }
+
+    public Card deal(){
+        if(unDealt.size() != 0){
+            Card card = unDealt.get(0);
+            dealt.add(card);
+            unDealt.remove(0);
+            return card;
+        }
+        else return null;
+    }
+    public void shuffle(){
+        while(dealt.size() > 0){
+            unDealt.add(dealt.get(0));
+            dealt.remove(0);
+
+        }
+        for(int i = 51; i > 0; i --){
+            int num = (int)(Math.random() * (i+1));
+            Card c = unDealt.get(num);
+            unDealt.set(num, unDealt.get(i));
+            unDealt.set(i, c);
+        }
+    }
 }
--- src/JacobCedar/Tester.java
+++ src/JacobCedar/Tester.java
@@ -3,6 +3,7 @@ package JacobCedar;
 public class Tester {
 
     public static void main(String[] args) {
-	// write your code here
+	    String[] ranks = new String[53];
+
     }
 }
